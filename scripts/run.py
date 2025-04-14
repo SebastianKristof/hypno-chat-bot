44365bc6c6a1759aa0fff5c58236b8199cbf3f99
Sebastian Kristof <svkristof@gmail.com>
2025-04-14 20:16:41 +0400

    some more python things

--- scripts/run.py
+++ scripts/run.py
@@ -7,6 +7,7 @@ import os
 # Add the src directory to the path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+# Modified import to use the correct module path
 from src.hypnobot.main import main
 
 if __name__ == "__main__":
